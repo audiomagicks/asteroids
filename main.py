d87467a0e0eb52e0cc113163629b67c101723858
audiomagicks <audiomagicks@gmail.com>
2025-01-05 13:48:54 -0600

    add shot class

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-import pygame
+import pygame # type: ignore
 from constants import *
 from player import *
 from asteroidfield import *
+from shot import *
 def main():
 	pygame.init()
 	clock = pygame.time.Clock()
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
 	asteroids = pygame.sprite.Group()
+	shots = pygame.sprite.Group()
 	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 	Asteroid.containers = (asteroids, updatable, drawable)
 	Player.containers = (updatable, drawable)
 	AsteroidField.containers = updatable
+	Shot.containers = (shots, updatable, drawable)
 
 	player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 	asteroidfield = AsteroidField()
@@ -23,17 +26,22 @@ def main():
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+			
 		screen.fill((0, 0, 0))
 		
 		for object in updatable:
 			object.update(dt)
+
 		for object in asteroids:
 			if object.collision(player):
 				print("Game over!")
 				exit()
+
 		for object in drawable:
 			object.draw(screen)
+			
 		pygame.display.flip()
+
 		dt = clock.tick(60) / 1000
 		
 
--- player.py
+++ player.py
@@ -1,6 +1,7 @@
-import pygame
+import pygame # type: ignore
 from constants import *
 from circleshape import *
+from shot import *
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -32,7 +33,15 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot(dt)
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self, dt):
+        shot = Shot(self.position.x, self.position.y)
+        vec = pygame.Vector2(0, 1).rotate(self.rotation)
+        shot.velocity = vec * PLAYER_SHOOT_SPEED
+        shot.add(Shot.containers)
